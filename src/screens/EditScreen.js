5eb164000d0ba0b2cc34c42b97bea4ea76276a24
Luis Juarez <luisisthejuarez@gmail.com>
2020-10-26 01:21:49 -0500

    Created edit screen

---
+++ src/screens/EditScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, Stylesheet } from "react-native";
+
+const EditScreen = () => {
+  return (
+    <View>
+      <Text>Edit Screen</Text>
+    </View>
+  );
+};
+
+export default EditScreen;
--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,25 +1,34 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import { Context } from "../context/BlogContext";
+import { Feather } from "@expo/vector-icons";
 
 const ShowScreen = ({ navigation }) => {
   const postID = navigation.getParam("id");
   const { state } = useContext(Context);
-  console.log(postID);
 
   const blogPost = state.find(
     (blogPost) => blogPost.id === navigation.getParam("id")
   );
 
-  console.log(state);
-  console.log(blogPost);
-
   return (
     <View>
       <Text>Show Screen</Text>
       <Text>{blogPost.title}</Text>
+      <Text>{blogPost.content}</Text>
     </View>
   );
+};
+
+ShowScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate("Edit")}>
+        <Feather name="edit" size={35} />
+      </TouchableOpacity>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({});
