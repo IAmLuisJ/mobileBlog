3503561c4f2be84c35327aec4f6ea1cd30632b3a
Luis Juarez <luisisthejuarez@gmail.com>
2020-10-27 00:18:12 -0500

    configured edit screen and initial state from id value

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,13 +1,47 @@
-import React from "react";
-import { View, Text, Stylesheet } from "react-native";
+import React, { useState, useContext } from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+import { Context } from "../context/BlogContext";
 
 const EditScreen = ({ navigation }) => {
+  const { state } = useContext(Context);
+  const postID = navigation.getParam("id");
+  const blogPost = state.find((item) => item.id === navigation.getParam("id"));
+  const [title, setTitle] = useState(blogPost.title);
+  const [content, setContent] = useState(blogPost.content);
+
   return (
     <View>
       <Text>Edit Screen</Text>
       <Text>{navigation.getParam("id")}</Text>
+      <Text style={styles.title}>Enter Title:</Text>
+      <TextInput
+        style={styles.textInput}
+        value={title}
+        onTextInput={(e) => setTitle(e)}
+      />
+      <Text style={styles.title}>Enter Content:</Text>
+      <TextInput
+        style={styles.textInput}
+        value={content}
+        onTextInput={(e) => setContent(e)}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    marginBottom: 10,
+    marginLeft: 5,
+  },
+  textInput: {
+    fontSize: 18,
+    margin: 5,
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 5,
+  },
+});
 
 export default EditScreen;
